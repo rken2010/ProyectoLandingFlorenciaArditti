3428926a39987bc7ff5f22d0fa2f03b8ac426886
rken2010 <rkenshin.2010@gmail.com>
2022-05-30 19:42:33 -0300

    envio formulario

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ function validacion(e){
     e.preventDefault()
     console.log("formulario enviado")
     swal("Consulta enviada!", "En breve me contactare con vos!", "success");
+    document.getElementById("form").submit();
     document.getElementById('form').reset(); 
 }
